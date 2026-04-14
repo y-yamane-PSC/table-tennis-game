c17df43c146c3f4bc157133a9198b675d796473b
Yuhei Yamane <y-yamane@pscsrv.co.jp>
2026-04-14 16:13:23 +0900

    再度ビルドをしてエラーの解消

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/docs/assets/',
+  base: './',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
   plugins: [react()],
 })
